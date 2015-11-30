9239915133b2e22517e0980e5a154a5399cb3f57
satodev <hemmi.satoru@gmail.com>
2015-11-30 16:06:09 +0100

    unrequired changes

--- classes/Authentification.class.php
+++ classes/Authentification.class.php
@@ -3,6 +3,31 @@ class Authentification
 {
 	public function __construct()
 	{
+		// $this->createPDOObject();
+	}
+	public function isDBinit()
+	{
+
+	}
+	public function startDB()
+	{
+		$this->createPDOObject();
+		$this->createDBName();
+		$this->createTable();
+	}
+	public function createPDOObject()
+	{
+		$host  = 'mysql:host=localhost:8080;';
+		$user = 'root';
+		$passwd = 'passwd';
+		$pdo_object = new PDO($host, $user, $passwd);
+	}
+	public function createDBName()
+	{
+
+	}
+	public function createTable()
+	{
 
 	}
 	public function login()
